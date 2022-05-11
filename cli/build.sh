3a872e58c8bf9a95584f328cb920515859f4e586
Guillaume Coré <gucore@redhat.com>
2022-05-11 18:03:01 +0200

    cli/build.sh: don't set -e
    Continue and do darwin too.

--- cli/build.sh
+++ cli/build.sh
@@ -4,6 +4,6 @@
 echo -n "Version? "
 read version
 export CGO_ENABLED=0
-set -x -e -u -o pipefail
+set -x -u -o pipefail
 gox --output="build/agnosticv_{{.OS}}_{{.Arch}}"  -ldflags="-X 'main.Version=${version}' -X 'main.buildTime=$(date -u)' -X 'main.buildCommit=$(git rev-parse HEAD)'"
 env GOOS=darwin GOARCH=arm64 go build -ldflags="-X 'main.Version=${version}' -X 'main.buildTime=$(date -u)' -X 'main.buildCommit=$(git rev-parse HEAD)'" -o build/agnosticv_darwin_arm64
