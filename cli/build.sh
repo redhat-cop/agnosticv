b7afefb1320e389f729a6aaf87d573fe2854e29f
Guillaume Coré <gucore@redhat.com>
2023-03-21 09:42:16 +0100

    build.sh: build into a dir per version

--- cli/build.sh
+++ cli/build.sh
@@ -5,5 +5,6 @@ echo -n "Version? "
 read version
 export CGO_ENABLED=0
 set -x -u -o pipefail
-gox --output="build/agnosticv_{{.OS}}_{{.Arch}}"  -ldflags="-X 'main.Version=${version}' -X 'main.buildTime=$(date -u)' -X 'main.buildCommit=$(git rev-parse HEAD)'"
-env GOOS=darwin GOARCH=arm64 go build -ldflags="-X 'main.Version=${version}' -X 'main.buildTime=$(date -u)' -X 'main.buildCommit=$(git rev-parse HEAD)'" -o build/agnosticv_darwin_arm64
+mkdir -p build/$version
+gox --output="build/${version}/agnosticv_{{.OS}}_{{.Arch}}"  -ldflags="-X 'main.Version=${version}' -X 'main.buildTime=$(date -u)' -X 'main.buildCommit=$(git rev-parse HEAD)'"
+env GOOS=darwin GOARCH=arm64 go build -ldflags="-X 'main.Version=${version}' -X 'main.buildTime=$(date -u)' -X 'main.buildCommit=$(git rev-parse HEAD)'" -o build/${version}/agnosticv_darwin_arm64
